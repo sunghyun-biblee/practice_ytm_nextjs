cd03007ef697cc53f60ac9259034c68df2632f13
sunghyun1014 <sunghyun1014@naver.com>
2024-09-30 21:37:53 +0900

    chore : 설정 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,13 @@ const config: Config = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1400px",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
